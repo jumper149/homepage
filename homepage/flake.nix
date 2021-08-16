ec5f1bfcf728fea89952937791aa25036d12121c
Felix Springer <felixspringer149@gmail.com>
2021-08-16 17:08:34 +0200

    Add nix flake.

---
+++ homepage/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Felix Springer's Homepage";
+
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixpkgs-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      (import ./nix/build.nix).homepage { inherit nix-gitignore haskellPackages; };
+
+    devShell.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+      (import ./nix/build.nix).homepageShell { inherit rnix-lsp haskellPackages nix-gitignore; };
+
+  };
+}
