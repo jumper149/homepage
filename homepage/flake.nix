afd9dcacb908f91a5ad8f93d098b24e70f209077
Felix Springer <felixspringer149@gmail.com>
2021-08-22 21:59:29 +0200

    Start with setting up nix for static files.

--- homepage/flake.nix
+++ homepage/flake.nix
@@ -16,6 +16,14 @@
       with import nixpkgs { system = "x86_64-linux"; };
       (import ./nix/build.nix).homepage { inherit haskellPackages nix-gitignore; };
 
+    packages.x86_64-linux.blog =
+      with import nixpkgs { system = "x86_64-linux"; };
+      (import ./nix/build.nix).blog { inherit asciidoctor stdenv; };
+
+    packages.x86_64-linux.static =
+      with import nixpkgs { system = "x86_64-linux"; };
+      (import ./nix/build.nix).static { inherit imagemagick stdenv; };
+
     devShell.x86_64-linux =
       with import nixpkgs { system = "x86_64-linux"; };
       (import ./nix/build.nix).homepageShell { inherit asciidoctor haskellPackages nix-gitignore rnix-lsp; };
