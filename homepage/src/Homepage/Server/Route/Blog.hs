c11d732bdbd9255fa1ed30ee77cf10ae5b63b9e1
Felix Springer <felixspringer149@gmail.com>
2021-08-24 15:00:20 +0200

    Serve blog files.

--- homepage/src/Homepage/Server/Route/Blog.hs
+++ homepage/src/Homepage/Server/Route/Blog.hs
@@ -2,13 +2,30 @@ module Homepage.Server.Route.Blog where
 
 import Homepage.Configuration
 import Homepage.Server.Err404
+import Homepage.Server.Html.Document
+import Homepage.Server.Tab
 
-import Servant
+import Servant hiding (serveDirectoryWith)
+import Servant.HTML.Blaze
+import Servant.RawM.Server
+import Text.Blaze.Html5
 import WaiAppStatic.Storage.Filesystem
 import WaiAppStatic.Types
 
-type API = Get '[JSON] String :<|> Raw
+type API = Get '[HTML] Html :<|> RawM
 
 handler :: MonadConfigured m => ServerT API m
-handler = pure "blog"
-     :<|> serveDirectoryWith (defaultFileServerSettings ".") { ss404Handler = Just application404 }
+handler = overviewHandler
+     :<|> articlesHandler
+
+overviewHandler :: Monad m
+                => m Html
+overviewHandler = pure $
+  document (Just TabBlog) $
+    h2 "my Blog"
+
+articlesHandler :: MonadConfigured m
+                => ServerT RawM m
+articlesHandler = do
+    path <- configDirectoryBlog <$> configuration
+    serveDirectoryWith (defaultFileServerSettings path) { ss404Handler = Just application404 }
