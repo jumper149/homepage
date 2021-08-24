92c85986dc850f0444a1b6c3aae580a6a4d463f9
Felix Springer <felixspringer149@gmail.com>
2021-08-24 14:11:18 +0200

    Add wrapper to flake.nix.

--- homepage/flake.nix
+++ homepage/flake.nix
@@ -14,12 +14,28 @@
 
     defaultPackage.x86_64-linux =
       with import nixpkgs { system = "x86_64-linux"; };
+      writeScript "homepage-full" ''
+        #!${pkgs.bash}/bin/bash
+        ${self.packages.x86_64-linux.homepage}/bin/homepage \
+          --directory-blog ${self.packages.x86_64-linux.blog}/static/blog \
+          --directory-files ${self.packages.x86_64-linux.files} \
+          --directory-static ${self.packages.x86_64-linux.static}
+      '';
+
+    packages.x86_64-linux.homepage =
+      with import nixpkgs { system = "x86_64-linux"; };
       (import ./nix/build.nix).homepage { inherit haskellPackages nix-gitignore; };
 
     packages.x86_64-linux.blog =
       with import nixpkgs { system = "x86_64-linux"; };
       (import ./nix/build.nix).blog { inherit asciidoctor stdenv; };
 
+    # TODO
+    packages.x86_64-linux.files =
+      with import nixpkgs { system = "x86_64-linux"; };
+      (import ./nix/build.nix).blog { inherit asciidoctor stdenv; };
+
+    # TODO
     packages.x86_64-linux.static =
       with import nixpkgs { system = "x86_64-linux"; };
       (import ./nix/build.nix).static { inherit imagemagick stdenv; };
