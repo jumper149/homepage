7da3f6d105172e0639673a3d36836523989cc80d
Felix Springer <felixspringer149@gmail.com>
2021-09-16 19:42:02 +0200

    Add ordering to displayed blog entries.

--- homepage/src/Homepage/Server/Html/Blog.hs
+++ homepage/src/Homepage/Server/Html/Blog.hs
@@ -2,7 +2,9 @@ module Homepage.Server.Html.Blog where
 
 import Homepage.Blog
 
+import Data.List
 import qualified Data.Map as M
+import Data.Ord
 import qualified Data.Text as T
 import Data.Time.Calendar
 import GHC.Generics
@@ -12,7 +14,7 @@ import qualified Text.Blaze.Html5 as H
 
 blogList :: BlogEntries -> Html
 blogList blogs = ul $
-  toMarkup $ entryToMarkup <$> M.toList (unBlogEntries blogs)
+  toMarkup $ entryToMarkup <$> sortOn (Down . blogTimestamp . snd) (M.toList (unBlogEntries blogs))
   where
     entryToMarkup (blogKey, BlogEntry { blogTitle, blogTimestamp }) =
       li $ p $ do
