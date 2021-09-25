e1731bdcad29356e876f903d4cad0808f4bbcba9
Felix Springer <felixspringer149@gmail.com>
2021-09-25 15:22:34 +0200

    Import RawM qualified.

--- homepage/src/Homepage/Server/Route/Static.hs
+++ homepage/src/Homepage/Server/Route/Static.hs
@@ -3,14 +3,14 @@ module Homepage.Server.Route.Static where
 import Homepage.Configuration
 import Homepage.Server.Err404
 
-import Servant hiding (serveDirectoryWith)
-import Servant.RawM.Server
+import Servant
+import qualified Servant.RawM.Server as RawM
 import WaiAppStatic.Storage.Filesystem
 import WaiAppStatic.Types
 
-type API = RawM
+type API = RawM.RawM
 
 handler :: MonadConfigured m => ServerT API m
 handler = do
   path <- configDirectoryStatic <$> configuration
-  serveDirectoryWith (defaultFileServerSettings path) { ss404Handler = Just application404 }
+  RawM.serveDirectoryWith (defaultFileServerSettings path) { ss404Handler = Just application404 }
