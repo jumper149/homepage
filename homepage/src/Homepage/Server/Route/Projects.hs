e0d4663cf2f4ea63c23695a5b630e577e85b410f
Felix Springer <felixspringer149@gmail.com>
2021-09-25 16:44:01 +0200

    Add simple page for projects.

--- homepage/src/Homepage/Server/Route/Projects.hs
+++ homepage/src/Homepage/Server/Route/Projects.hs
@@ -1,9 +1,38 @@
 module Homepage.Server.Route.Projects where
 
+import Homepage.Server.Html.Document
+import Homepage.Configuration
+import Homepage.Server.Tab
+
 import Servant
+import Servant.HTML.Blaze
+import Text.Blaze.Html5
+import Text.Blaze.Html5.Attributes
 
-type API = Get '[JSON] String
+type API = Get '[HTML] Html
 
-handler :: Monad m
+handler :: MonadConfigured m
         => ServerT API m
-handler = pure "projects"
+handler = do
+  baseUrl <- configBaseUrl <$> configuration
+  pure $ document baseUrl (Just 0) (Just TabProjects) $ do
+    h2 "my Projects"
+    ul $ do
+      li $ do
+        "A blue light filter in the style of XMonad: "
+        a ! href "https://github.com/jumper149/blucontrol" $ "blucontrol"
+        " replaces blugon for my personal usage"
+      li $ do
+        "The game "
+        a ! href "https://github.com/jumper149/go" $ "go"
+        " providing a smooth multiplayer browser experience with a webserver"
+      li $ do
+        "A simple Blue Light Filter for X: "
+        a ! href "https://github.com/jumper149/blugon" $ "blugon"
+      li $ do
+        "Compiler from I- to S-Expression for the Scheme Programming Language: "
+        a ! href "https://github.com/jumper149/haskeme" $ "haskeme"
+      li $ do
+        "The "
+        a ! href "https://github.com/jumper149/dotfiles" $ "dotfiles"
+        " to configure my ArchLinux-Systems"
