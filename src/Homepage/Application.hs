9c54d090819a1e6c1e570a481add23e74a8c8ce9
Felix Springer <felixspringer149@gmail.com>
2021-09-26 01:34:01 +0200

    Add MonadLogger instance.

--- src/Homepage/Application.hs
+++ src/Homepage/Application.hs
@@ -9,15 +9,19 @@ import Homepage.Application.Configured
 import Control.Monad.Base
 import Control.Monad.Catch
 import Control.Monad.Except
+import Control.Monad.Identity
 import Control.Monad.Logger
 import Control.Monad.Trans.Control
 import Control.Monad.Trans.Reader
 
-newtype ApplicationT m a = ApplicationT { unApplicationT :: (ConfiguredT |. LoggingT) m a }
+newtype ApplicationT m a = ApplicationT { unApplicationT :: (ConfiguredT |. LoggingT |. IdentityT) m a }
   deriving newtype (Applicative, Functor, Monad, MonadBase b, MonadBaseControl b, MonadTrans, MonadTransControl, MonadThrow, MonadCatch, MonadError e)
 
 instance Monad m => MonadConfigured (ApplicationT m) where
   configuration = ApplicationT $ ComposeT configuration
+
+instance MonadIO m => MonadLogger (ApplicationT m) where
+  monadLoggerLog loc logSource logLevel = ApplicationT . ComposeT . lift . ComposeT . monadLoggerLog loc logSource logLevel
 
 runApplication :: MonadIO m
                => ApplicationT m a
@@ -26,4 +30,4 @@ runApplication app = do
   config <- liftIO launch
   let
     runConfiguredT' tma = runConfiguredT tma config
-  runConfiguredT' |.| runStdoutLoggingT $ unApplicationT app
+  runConfiguredT' |.| runStdoutLoggingT |.| runIdentityT $ unApplicationT app
