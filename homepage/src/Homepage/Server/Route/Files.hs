8a77c91e64da0ba8f0bdb2feea20b9e8cce35b03
Felix Springer <felixspringer149@gmail.com>
2021-09-25 17:03:09 +0200

    Add simple files page.

--- homepage/src/Homepage/Server/Route/Files.hs
+++ homepage/src/Homepage/Server/Route/Files.hs
@@ -1,16 +1,57 @@
 module Homepage.Server.Route.Files where
 
 import Homepage.Configuration
+import Homepage.Server.Html.Depth
+import Homepage.Server.Html.Document
 import Homepage.Server.Err404
 
 import Servant
+import Servant.HTML.Blaze
 import qualified Servant.RawM.Server as RawM
+import Text.Blaze.Html5
+import Text.Blaze.Html5.Attributes
+import qualified Text.Blaze.Html5 as H
 import WaiAppStatic.Storage.Filesystem
 import WaiAppStatic.Types
 
-type API = RawM.RawM
+type API = Get '[HTML] Html
+      :<|> RawM.RawM
 
-handler :: MonadConfigured m => ServerT API m
-handler = do
-  dir <- configDirectoryFiles <$> configuration
-  RawM.serveDirectoryWith (defaultFileServerSettings dir) { ss404Handler = Just application404 }
+handler :: MonadConfigured m
+        => ServerT API m
+handler = overviewHandler :<|> filesHandler
+
+overviewHandler :: MonadConfigured m
+                => m Html
+overviewHandler = do
+  baseUrl <- configBaseUrl <$> configuration
+  pure $ document baseUrl (Just 0) Nothing $ do
+    h2 "my Files"
+    ul $ do
+      li $ do
+        "curriculum vitae (24/06/21) "
+        H.span ! class_ "options" $ do
+            "[ "
+            a ! hrefWithDepth baseUrl (Just 0) "files/Felix_Springer-cv.pdf" $ "PDF"
+            " ]"
+      li $ do
+        "mail signing key (02/09/20) "
+        H.span ! class_ "options" $ do
+            "[ "
+            a ! hrefWithDepth baseUrl (Just 0) "files/Felix_Springer-publickey-mail.gpg" $ "GPG"
+            " ]"
+    h2 "my Publications"
+    ul $ do
+      li $ do
+        "Storage Register Design for an Ion Trap Quantum Processor (07/04/21) "
+        H.span ! class_ "options" $ do
+          "[ "
+          a ! hrefWithDepth baseUrl (Just 0) "files/Felix_Springer-Storage_Register_Design_for_an_Ion_Trap_Quantum_Processor.pdf" $ "PDF"
+          " ]"
+
+
+filesHandler :: MonadConfigured m
+             => ServerT RawM.RawM m
+filesHandler = do
+  directory <- configDirectoryFiles <$> configuration
+  RawM.serveDirectoryWith (defaultFileServerSettings directory) { ss404Handler = Just application404 }
