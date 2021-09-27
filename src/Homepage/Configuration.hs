693bc673e881b564b282917ffff861e759c4ec70
Felix Springer <felixspringer149@gmail.com>
2021-09-27 19:25:07 +0200

    Reorder configuration options with descending priority.

--- src/Homepage/Configuration.hs
+++ src/Homepage/Configuration.hs
@@ -11,9 +11,9 @@ data Configuration = Configuration
     { configDirectoryBlog :: FilePath
     , configDirectoryFiles :: FilePath
     , configDirectoryStatic :: FilePath
-    , configBlogEntries :: BlogEntries
     , configPort :: Word16
     , configBaseUrl :: T.Text
+    , configBlogEntries :: BlogEntries
     }
   deriving stock (Eq, Generic, Ord, Read, Show)
   deriving anyclass (A.FromJSON, A.ToJSON)
