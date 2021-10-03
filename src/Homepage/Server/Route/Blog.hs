c3adc0b3a8a03d8b4c70c7b2b6022ab7aff6b2fe
Felix Springer <felixspringer149@gmail.com>
2021-10-03 15:38:59 +0200

    Change blog article site to just show a HTML preview.

--- src/Homepage/Server/Route/Blog.hs
+++ src/Homepage/Server/Route/Blog.hs
@@ -20,11 +20,14 @@ import qualified Data.Text as T
 import Servant hiding (serveDirectoryWith)
 import Servant.HTML.Blaze
 import Text.Blaze.Html5
+import Text.Blaze.Html5.Attributes
+import qualified Text.Blaze.Html5.Attributes as H
 
 type API = "atom.xml" :> Atom.API
       :<|> Capture "article" T.Text :>
         (    Get '[HTML] Html
-        :<|> (    "adoc" :> Get '[HTML] Html
+        :<|> (    "html" :> Get '[HTML] Html
+             :<|> "adoc" :> Get '[HTML] Html
              :<|> "pdf" :> Get '[HTML] Html
              )
         )
@@ -52,8 +55,43 @@ overviewHandler = do
 
 articlesHandler :: (MonadBlog m, MonadConfigured m, MonadError ServerError m, MonadLogger m)
                 => T.Text
-                -> (m Html :<|> m Html :<|> m Html)
-articlesHandler articleKey = htmlHandler articleKey :<|> undefined :<|> undefined
+                -> (m Html :<|> m Html :<|> m Html :<|> m Html)
+articlesHandler articleKey = wrapperHandler articleKey :<|> htmlHandler articleKey :<|> undefined :<|> undefined
+
+wrapperHandler :: (MonadBlog m, MonadConfigured m, MonadError ServerError m, MonadLogger m)
+               => T.Text
+               -> m Html
+wrapperHandler articleKey = do
+  baseUrl <- configBaseUrl <$> configuration
+  blogs <- configBlogEntries <$> configuration
+  case lookupBlog articleKey blogs of
+    Nothing -> do
+        $logError $ "Failed to serve blog article: " <> T.pack (show articleKey)
+        servantError404
+    Just blog -> do
+        $logInfo $ "Serve blog article: " <> T.pack (show articleKey)
+        pure $ document baseUrl (Just 1) (Just TabBlog) $ do
+          h2 $ text $ blogTitle blog
+          p $ do
+            "View blog entry: "
+            a ! hrefWithDepth baseUrl (Just 1) (textValue $ "blog/" <> articleKey <> "/html") $ "HTML"
+            " | "
+            a ! hrefWithDepth baseUrl (Just 1) (textValue $ "blog/" <> articleKey <> "/pdf") $ "PDF"
+            " | "
+            a ! hrefWithDepth baseUrl (Just 1) (textValue $ "blog/" <> articleKey <> "/adoc") $ "ADOC"
+          p "Preview below."
+          hr
+          script ! type_ "text/javascript" $
+            "function resizeIframe(iframe) {\
+            \  iframe.height = iframe.contentWindow.document.body.scrollHeight + \"px\";\
+            \}"
+          iframe ! src (textValue $ "./" <> articleKey <> "/html")
+                 ! name "blog article (HTML)"
+                 ! width "100%"
+                 ! H.onload "resizeIframe(this)"
+                 ! target "_parent"
+                 ! H.style "border: none;"
+                 $ mempty
 
 htmlHandler :: (MonadBlog m, MonadConfigured m, MonadError ServerError m, MonadLogger m)
             => T.Text
@@ -69,5 +107,4 @@ htmlHandler articleKey = do
         $logInfo $ "Read blog article from file: " <> T.pack (show blog)
         content <- readBlogEntry BlogFormatHTML blog
         $logInfo $ "Serve HTML blog article: " <> T.pack (show articleKey)
-        pure $ document baseUrl (Just 1) (Just TabBlog) $
-          toMarkup $ AsciiDocHtml content
+        pure $ toMarkup $ AsciiDocHtml content
