88ea003fba22fa70eef07c87a624cb31635c74b9
Felix Springer <felixspringer149@gmail.com>
2021-10-05 23:04:30 +0200

    Use function for duplicate code.

--- src/Homepage/Server/Err404.hs
+++ src/Homepage/Server/Err404.hs
@@ -7,6 +7,7 @@ import Homepage.Server.Html.Document
 
 import Control.Monad.Error.Class
 import Data.ByteString.Builder
+import Data.Text as T
 import Servant
 import Network.HTTP.Types.Status
 import Network.Wai
@@ -19,11 +20,7 @@ application404 = do
   baseUrl <- configBaseUrl <$> configuration
   pure $ \ _ rsp -> rsp $
     responseBuilder status404 [ (,) "Content-Type" "text/html" ] $
-      lazyByteString $ renderHtml $
-        document baseUrl Nothing Nothing $ do
-          h1 "404"
-          h2 "You got lost?"
-          p $ "My homepage is " <> (a ! hrefWithDepth baseUrl Nothing "" $ "here") <> "."
+      lazyByteString $ renderHtml $ html404 baseUrl
 
 servantError404 :: (MonadConfigured m, MonadError ServerError m)
                 => m a
@@ -31,9 +28,12 @@ servantError404 = do
   baseUrl <- configBaseUrl <$> configuration
   throwError err404
     { errHeaders = [ (,) "Content-Type" "text/html" ]
-    , errBody = renderHtml $
-        document baseUrl Nothing Nothing $ do
-          h1 "404"
-          h2 "You got lost?"
-          p $ "My homepage is " <> (a ! hrefWithDepth baseUrl Nothing "" $ "here") <> "."
+    , errBody = renderHtml $ html404 baseUrl
     }
+
+html404 :: T.Text -> Html
+html404 baseUrl =
+  document baseUrl Nothing Nothing $ do
+    h1 "404"
+    h2 "You got lost?"
+    p $ "My homepage is " <> (a ! hrefWithDepth baseUrl Nothing "" $ "here") <> "."
