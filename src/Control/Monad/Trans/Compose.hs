01c6ed14b5b928a3bb49399bd276c1663cd19922
Felix Springer <felixspringer149@gmail.com>
2021-12-18 19:53:23 +0100

    Rename unComposeT to deComposeT.

--- src/Control/Monad/Trans/Compose.hs
+++ src/Control/Monad/Trans/Compose.hs
@@ -18,7 +18,7 @@ newtype ComposeT
   (t2 :: (Type -> Type) -> Type -> Type)
   (m :: Type -> Type)
   (a :: Type)
-    = ComposeT { unComposeT :: t1 (t2 m) a }
+    = ComposeT { deComposeT :: t1 (t2 m) a }
   deriving newtype (Applicative, Functor, Monad)
 
 instance (forall m. Monad m => Monad (t2 m), MonadTrans t1, MonadTrans t2) => MonadTrans (ComposeT t1 t2) where
@@ -26,7 +26,7 @@ instance (forall m. Monad m => Monad (t2 m), MonadTrans t1, MonadTrans t2) => Mo
 
 instance (forall m. Monad m => Monad (t2 m), MonadTransControl t1, MonadTransControl t2) => MonadTransControl (ComposeT t1 t2) where
   type StT (ComposeT t1 t2) a = StT t2 (StT t1 a)
-  liftWith f = defaultLiftWith2 ComposeT unComposeT $ \x -> f x
+  liftWith f = defaultLiftWith2 ComposeT deComposeT $ \x -> f x
   restoreT = defaultRestoreT2 ComposeT
 
 instance (Monad (t1 (t2 m)), MonadTrans (ComposeT t1 t2), MonadIO m) => MonadIO (ComposeT t1 t2 m) where
@@ -44,15 +44,15 @@ instance (Monad (t1 (t2 m)), MonadTrans t1, MonadThrow (t2 m)) => MonadThrow (Co
   throwM = ComposeT . lift . throwM
 
 instance (Monad (t1 (t2 m)), MonadTransControl t1, MonadCatch (t2 m)) => MonadCatch (ComposeT t1 t2 m) where
-  catch throwing catching = ComposeT $ restoreT . pure =<< liftWith (\ runT -> catch (runT $ unComposeT throwing) (runT . unComposeT . catching))
+  catch throwing catching = ComposeT $ restoreT . pure =<< liftWith (\ runT -> catch (runT $ deComposeT throwing) (runT . deComposeT . catching))
 
 instance (Monad (t1 (t2 m)), MonadTransControl t1, MonadError e (t2 m)) => MonadError e (ComposeT t1 t2 m) where
   throwError = ComposeT . lift . throwError
-  catchError throwing catching = ComposeT $ restoreT . pure =<< liftWith (\ runT -> catchError (runT $ unComposeT throwing) (runT . unComposeT . catching))
+  catchError throwing catching = ComposeT $ restoreT . pure =<< liftWith (\ runT -> catchError (runT $ deComposeT throwing) (runT . deComposeT . catching))
 
 instance (Monad (t1 (t2 m)), MonadTransControl t1, MonadReader r (t2 m)) => MonadReader r (ComposeT t1 t2 m) where
   ask = ComposeT $ lift ask
-  local f tma = ComposeT $ restoreT . pure =<< liftWith (\ runT -> Control.Monad.Reader.Class.local f $ runT $ unComposeT tma)
+  local f tma = ComposeT $ restoreT . pure =<< liftWith (\ runT -> Control.Monad.Reader.Class.local f $ runT $ deComposeT tma)
 
 instance (Monad (t1 (t2 m)), MonadTrans t1, MonadState s (t2 m)) => MonadState s (ComposeT t1 t2 m) where
   get = ComposeT $ lift get
@@ -60,15 +60,15 @@ instance (Monad (t1 (t2 m)), MonadTrans t1, MonadState s (t2 m)) => MonadState s
 
 instance (Monad (t1 (t2 m)), MonadTransControl t1, MonadWriter w (t2 m)) => MonadWriter w (ComposeT t1 t2 m) where
   tell = ComposeT . lift . tell
-  listen tma = ComposeT $ (\ (sta, w) -> (, w) <$> restoreT (pure sta)) =<< liftWith (\ runT -> listen $ runT $ unComposeT tma)
-  pass tma = ComposeT $ lift . pass . pure =<< unComposeT tma
+  listen tma = ComposeT $ (\ (sta, w) -> (, w) <$> restoreT (pure sta)) =<< liftWith (\ runT -> listen $ runT $ deComposeT tma)
+  pass tma = ComposeT $ lift . pass . pure =<< deComposeT tma
 
 runComposeT :: (forall a. t1 (t2 m) a -> t2 m (StT t1 a))
             -> (forall a. t2 m a -> m (StT t2 a))
             -> (forall a. ComposeT t1 t2 m a -> m (StT t2 (StT t1 a)))
-runComposeT runT1 runT2 = runT2 . runT1 . unComposeT
+runComposeT runT1 runT2 = runT2 . runT1 . deComposeT
 
 runComposeT' :: (t1 (t2 m) a -> t2 m a)
              -> (t2 m a -> m a)
              -> (ComposeT t1 t2 m a -> m a)
-runComposeT' runT1 runT2 = runT2 . runT1 . unComposeT
+runComposeT' runT1 runT2 = runT2 . runT1 . deComposeT
