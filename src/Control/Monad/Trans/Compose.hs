6a4aa04a634e2e487f962aedcb3ea49b5ecc4990
Felix Springer <felixspringer149@gmail.com>
2021-12-19 14:47:04 +0100

    Use `Elevator` to derive instance for `ComposeT`.

--- src/Control/Monad/Trans/Compose.hs
+++ src/Control/Monad/Trans/Compose.hs
@@ -30,37 +30,54 @@ instance (forall m. Monad m => Monad (t2 m), MonadTransControl t1, MonadTransCon
   liftWith f = defaultLiftWith2 ComposeT deComposeT $ \x -> f x
   restoreT = defaultRestoreT2 ComposeT
 
--- | Elevated.
+-- | Elevated to `m`.
 deriving via (Elevator (ComposeT t1 t2) m) instance (Monad (t1 (t2 m)), MonadTrans (ComposeT t1 t2), MonadIO m) => MonadIO (ComposeT t1 t2 m)
 
--- | Elevated.
+-- | Elevated to `m`.
 deriving via (Elevator (ComposeT t1 t2) m) instance (Monad (t1 (t2 m)), MonadTrans (ComposeT t1 t2), MonadBase b m) => MonadBase b (ComposeT t1 t2 m)
 
--- | Elevated.
+-- | Elevated to `m`.
 deriving via (Elevator (ComposeT t1 t2) m) instance (Monad (t1 (t2 m)), MonadTransControl (ComposeT t1 t2), MonadBaseControl b m) => MonadBaseControl b (ComposeT t1 t2 m)
 
+-- | Elevated to `t2 m`.
 instance (Monad (t1 (t2 m)), MonadTrans t1, MonadThrow (t2 m)) => MonadThrow (ComposeT t1 t2 m) where
   throwM = ComposeT . lift . throwM
 
+-- | Elevated to `t2 m`.
 instance (Monad (t1 (t2 m)), MonadTransControl t1, MonadCatch (t2 m)) => MonadCatch (ComposeT t1 t2 m) where
   catch throwing catching = ComposeT $ restoreT . pure =<< liftWith (\ runT -> catch (runT $ deComposeT throwing) (runT . deComposeT . catching))
 
-instance (Monad (t1 (t2 m)), MonadTransControl t1, MonadError e (t2 m)) => MonadError e (ComposeT t1 t2 m) where
-  throwError = ComposeT . lift . throwError
-  catchError throwing catching = ComposeT $ restoreT . pure =<< liftWith (\ runT -> catchError (runT $ deComposeT throwing) (runT . deComposeT . catching))
+-- | Elevated to `t2 m`.
+deriving via Elevator t1 (t2 (m :: * -> *))
+  instance
+    ( Monad (t1 (t2 m))
+    , MonadTransControl t1
+    , MonadError e (t2 m)
+    ) => MonadError e (ComposeT t1 t2 m)
 
-instance (Monad (t1 (t2 m)), MonadTransControl t1, MonadReader r (t2 m)) => MonadReader r (ComposeT t1 t2 m) where
-  ask = ComposeT $ lift ask
-  local f tma = ComposeT $ restoreT . pure =<< liftWith (\ runT -> Control.Monad.Reader.Class.local f $ runT $ deComposeT tma)
+-- | Elevated to `t2 m`.
+deriving via Elevator t1 (t2 (m :: * -> *))
+  instance
+    ( Monad (t1 (t2 m))
+    , MonadTransControl t1
+    , MonadReader r (t2 m)
+    ) => MonadReader r (ComposeT t1 t2 m)
 
-instance (Monad (t1 (t2 m)), MonadTrans t1, MonadState s (t2 m)) => MonadState s (ComposeT t1 t2 m) where
-  get = ComposeT $ lift get
-  put = ComposeT . lift . put
+-- | Elevated to `t2 m`.
+deriving via Elevator t1 (t2 (m :: * -> *))
+  instance
+    ( Monad (t1 (t2 m))
+    , MonadTrans t1
+    , MonadState s (t2 m)
+    ) => MonadState s (ComposeT t1 t2 m)
 
-instance (Monad (t1 (t2 m)), MonadTransControl t1, MonadWriter w (t2 m)) => MonadWriter w (ComposeT t1 t2 m) where
-  tell = ComposeT . lift . tell
-  listen tma = ComposeT $ (\ (sta, w) -> (, w) <$> restoreT (pure sta)) =<< liftWith (\ runT -> listen $ runT $ deComposeT tma)
-  pass tma = ComposeT $ lift . pass . pure =<< deComposeT tma
+-- | Elevated to `t2 m`.
+deriving via Elevator t1 (t2 (m :: * -> *))
+  instance
+    ( Monad (t1 (t2 m))
+    , MonadTransControl t1
+    , MonadWriter w (t2 m)
+    ) => MonadWriter w (ComposeT t1 t2 m)
 
 runComposeT :: (forall a. t1 (t2 m) a -> t2 m (StT t1 a))
             -> (forall a. t2 m a -> m (StT t2 a))
