9174994f0c5dabd70e1ea8fb47957116a4a2f1d5
Felix Springer <felixspringer149@gmail.com>
2021-12-20 23:26:12 +0100

    Add hover titles to icons.

--- src/Homepage/Server/Html/Header.hs
+++ src/Homepage/Server/Html/Header.hs
@@ -9,6 +9,7 @@ import Numeric.Natural
 import Text.Blaze.Html5
 import Text.Blaze.Html5.Attributes
 import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 
 headerTab :: T.Text -- ^ base URL
           -> Maybe Natural -- ^ depth
@@ -41,7 +42,7 @@ headerTabs baseUrl depth activeTab =
     headerTabsHelper baseUrl depth activeTab
     H.span $ do
       a ! hrefWithDepth baseUrl depth "blog/atom.xml" ! class_ "icon" $
-        img ! src (withDepth baseUrl depth "icons/feed.png") ! class_ "icon"
+        img ! src (withDepth baseUrl depth "icons/feed.png") ! A.title "Feed" ! class_ "icon"
       a ! href "https://github.com/jumper149" ! class_ "icon" $
-        img ! src (withDepth baseUrl depth "icons/GitHub.png") ! class_ "icon"
+        img ! src (withDepth baseUrl depth "icons/GitHub.png") ! A.title "GitHub" ! class_ "icon"
       a ! href (textValue baseUrl) $ "felixspringer.xyz" -- TODO: base URL naming is hardcoded
