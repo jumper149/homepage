e48347637ce756b769b97665383f2c0881578ed0
Felix Springer <felixspringer149@gmail.com>
2022-01-29 10:58:42 +0100

    Log when starting the server.

--- src/Homepage/Server.hs
+++ src/Homepage/Server.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Homepage.Server where
 
 import Homepage.Application
@@ -5,12 +7,14 @@ import Homepage.Application.Configured
 import Homepage.Configuration
 import Homepage.Server.Route
 
+import Control.Monad.Logger
 import Control.Monad.Trans.Control
 import Network.Wai.Handler.Warp
 import Servant.Server.Generic
 
 server :: ApplicationT IO ()
 server = do
+  $logInfo "Start server."
   port <- configPort <$> configuration
   liftWith $ \ runT ->
     run (fromEnum port) $ genericServeT runT routes
