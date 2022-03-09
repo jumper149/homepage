062b396b68414e06f555943d47f119917dbebb84
Felix Springer <felixspringer149@gmail.com>
2022-03-09 01:01:48 +0100

    Remove unused utility function to access environment variables by their name.

--- src/Homepage/Application/Environment/Class.hs
+++ src/Homepage/Application/Environment/Class.hs
@@ -5,7 +5,6 @@ import Homepage.Environment
 import Control.Monad.Trans
 import Control.Monad.Trans.Compose
 import Control.Monad.Trans.Elevator
-import Data.Proxy
 
 class Monad m => MonadEnvironment m where
   environment :: m Environment
@@ -22,8 +21,3 @@ deriving via Elevator t1 (t2 (m :: * -> *))
     , MonadTrans t1
     , MonadEnvironment (t2 m)
     ) => MonadEnvironment (ComposeT t1 t2 m)
-
-environmentVariable :: (EnvironmentVariable envVar, MonadEnvironment m)
-                    => Proxy envVar
-                    -> m (EnvironmentVariableContent envVar)
-environmentVariable proxy = askEnvironmentVariable proxy <$> environment
