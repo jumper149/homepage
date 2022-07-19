b746d0a98215510615f8f0257fff55761a3fbf99
Felix Springer <felixspringer149@gmail.com>
2022-07-19 22:04:55 +0200

    Wrap `IdentityT` for `EmptyT`.
    This prevents you from running this accidentaly with `runIdentityT`.

--- source/library/Control/Monad/Trans/Compose/Empty.hs
+++ source/library/Control/Monad/Trans/Compose/Empty.hs
@@ -1,9 +1,18 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 module Control.Monad.Trans.Compose.Empty where
 
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Control
+import Control.Monad.Trans.Control.Identity
 import Control.Monad.Trans.Elevator
 import Control.Monad.Trans.Identity
 
-type EmptyT = Elevator IdentityT
+type EmptyT = Elevator NoT
 
 runEmptyT :: EmptyT m a -> m a
-runEmptyT = runIdentityT . descend
+runEmptyT = runIdentityT . runNoT . descend
+
+newtype NoT m a = MkNoT {runNoT :: IdentityT m a}
+  deriving newtype (Functor, Applicative, Monad)
+  deriving newtype (MonadTrans, MonadTransControl, MonadTransControlIdentity)
