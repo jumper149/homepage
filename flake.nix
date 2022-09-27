db28049b1449bab12cad10208126c6f1da8c2670
Felix Springer <felixspringer149@gmail.com>
2022-09-27 21:38:16 +0200

    Add subflake for static files.

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
       server = import ./server/subflake.nix { inherit nixpkgs setup; };
       blog = import ./blog/subflake.nix { inherit nixpkgs setup; };
       files = import ./files/subflake.nix { inherit nixpkgs setup; };
+      static = import ./static/subflake.nix { inherit nixpkgs setup; };
     };
 
     checks.x86_64-linux.subflakes =
@@ -46,70 +47,7 @@
         revision = if self ? rev then self.rev else null;
         directory-blog = "${self.subflakes.blog.packages.x86_64-linux.default}";
         directory-files = "${self.subflakes.files.packages.x86_64-linux.default}";
-        directory-static = "${self.packages.x86_64-linux.static}";
-      };
-
-    packages.x86_64-linux.static =
-      with import nixpkgs { system = "x86_64-linux"; overlays = [ self.subflakes.setup.overlays.default ]; };
-      stdenv.mkDerivation {
-        name = "static"; # TODO: Necessary to avoid segmentation fault.
-        src = ./static/static;
-        buildPhase = ''
-          FAVICON_RESOLUTIONS=(
-            "32x32"
-            "192x192"
-            "512x512"
-          )
-          for resolution in ''${FAVICON_RESOLUTIONS[*]}
-          do
-            echo "Build 'favicon-''${resolution}.png'."
-            convert favicon.xpm -scale "''${resolution}" "favicon-''${resolution}.png"
-          done
-
-          echo "Remove 'favicon.xpm'."
-          rm favicon.xpm
-
-          echo "Add link 'favicon.png'."
-          ln -s favicon-32x32.png favicon.png
-
-          echo "Add link 'favicon.ico'."
-          ln -s favicon.png favicon.ico
-
-          resizeIcon() {
-            ICON_NAME="$1"
-            if [ -f "$ICON_NAME" ]
-            then
-              echo "Icon exists: '$ICON_NAME'."
-
-              echo "Resize icon: '$ICON_NAME'."
-              convert "$ICON_NAME" -resize 128x128 "$ICON_NAME"
-            else
-              echo "Icon doesn't exist: '$ICON_NAME'"
-              exit 2
-            fi
-          }
-
-          for icon in ./icons/*
-          do
-            resizeIcon "$icon"
-          done
-
-          echo "Compile CSS."
-          lessc stylesheet.less stylesheet.css
-
-          echo "Remove 'stylesheet.less'."
-          rm stylesheet.less
-        '';
-        installPhase = ''
-          mkdir -p $out
-          cp --recursive . $out
-        '';
-        buildInputs = [
-        ];
-        nativeBuildInputs = [
-          imagemagick
-          lessc
-        ];
+        directory-static = "${self.subflakes.static.packages.x86_64-linux.static}";
       };
 
     devShells.x86_64-linux.default =
---
+++ static/subflake.nix
@@ -0,0 +1,66 @@
+{ nixpkgs, setup }: {
+
+  packages.x86_64-linux.static =
+    with import nixpkgs { system = "x86_64-linux"; overlays = [ setup.overlays.default ]; };
+    stdenv.mkDerivation {
+      name = "static"; # TODO: Necessary to avoid segmentation fault.
+      src = ./static;
+      buildPhase = ''
+        FAVICON_RESOLUTIONS=(
+          "32x32"
+          "192x192"
+          "512x512"
+        )
+        for resolution in ''${FAVICON_RESOLUTIONS[*]}
+        do
+          echo "Build 'favicon-''${resolution}.png'."
+          convert favicon.xpm -scale "''${resolution}" "favicon-''${resolution}.png"
+        done
+
+        echo "Remove 'favicon.xpm'."
+        rm favicon.xpm
+
+        echo "Add link 'favicon.png'."
+        ln -s favicon-32x32.png favicon.png
+
+        echo "Add link 'favicon.ico'."
+        ln -s favicon.png favicon.ico
+
+        resizeIcon() {
+          ICON_NAME="$1"
+          if [ -f "$ICON_NAME" ]
+          then
+            echo "Icon exists: '$ICON_NAME'."
+
+            echo "Resize icon: '$ICON_NAME'."
+            convert "$ICON_NAME" -resize 128x128 "$ICON_NAME"
+          else
+            echo "Icon doesn't exist: '$ICON_NAME'"
+            exit 2
+          fi
+        }
+
+        for icon in ./icons/*
+        do
+          resizeIcon "$icon"
+        done
+
+        echo "Compile CSS."
+        lessc stylesheet.less stylesheet.css
+
+        echo "Remove 'stylesheet.less'."
+        rm stylesheet.less
+      '';
+      installPhase = ''
+        mkdir -p $out
+        cp --recursive . $out
+      '';
+      buildInputs = [
+      ];
+      nativeBuildInputs = [
+        imagemagick
+        lessc
+      ];
+    };
+
+}
