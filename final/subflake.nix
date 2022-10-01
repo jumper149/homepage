033e73309983d45c1a00e7525d18afb04c0ac34a
Felix Springer <felixspringer149@gmail.com>
2022-10-01 21:18:31 +0200

    Move initialization test into final subflake.

--- final/subflake.nix
+++ final/subflake.nix
@@ -48,4 +48,39 @@
         };
       };
     };
+
+  packages.x86_64-linux.homepage-test-application =
+    with import nixpkgs { system = "x86_64-linux"; overlays = [ setup.overlays.default ]; };
+    writeScriptBin "homepage-test-application-full" ''
+      export HOMEPAGE_CONFIG_FILE="${config.packages.x86_64-linux.default}"
+      export HOMEPAGE_LOG_LEVEL=LevelWarn
+      ${server.packages.x86_64-linux.test-application}/bin/homepage-test-application
+    '';
+
+  checks.x86_64-linux.homepage-test-application =
+    with import nixpkgs { system = "x86_64-linux"; overlays = [ setup.overlays.default ]; };
+    stdenv.mkDerivation {
+      name = "homepage-test-application"; # TODO: Necessary to avoid segmentation fault.
+      src = ./.;
+      buildPhase = ''
+        INIT_LOG="$(homepage-test-application-full)"
+        echo "$INIT_LOG"
+
+        if [ -z "$INIT_LOG" ];
+        then
+          echo "Successfully checked the initialization log."
+        else
+          echo "Warnings/Errors/Unknowns detected in initialization log."
+          echo "$INIT_LOG"
+          exit 1
+        fi
+      '';
+      installPhase = ''
+        mkdir $out
+      '';
+      buildInputs = [
+        packages.x86_64-linux.homepage-test-application
+      ];
+    };
+
 }
--- flake.nix
+++ flake.nix
@@ -49,39 +49,5 @@
 
     nixosModules.default = self.subflakes.final.nixosModules.default;
 
-    packages.x86_64-linux.homepage-test-application =
-      with import nixpkgs { system = "x86_64-linux"; overlays = [ self.subflakes.setup.overlays.default ]; };
-      writeScriptBin "homepage-test-application-full" ''
-        export HOMEPAGE_CONFIG_FILE="${self.subflakes.config.packages.x86_64-linux.default}"
-        export HOMEPAGE_LOG_LEVEL=LevelWarn
-        ${self.subflakes.server.packages.x86_64-linux.test-application}/bin/homepage-test-application
-      '';
-
-    checks.x86_64-linux.homepage-test-application =
-      with import nixpkgs { system = "x86_64-linux"; overlays = [ self.subflakes.setup.overlays.default ]; };
-      stdenv.mkDerivation {
-        name = "homepage-test-application"; # TODO: Necessary to avoid segmentation fault.
-        src = ./.;
-        buildPhase = ''
-          INIT_LOG="$(homepage-test-application-full)"
-          echo "$INIT_LOG"
-
-          if [ -z "$INIT_LOG" ];
-          then
-            echo "Successfully checked the initialization log."
-          else
-            echo "Warnings/Errors/Unknowns detected in initialization log."
-            echo "$INIT_LOG"
-            exit 1
-          fi
-        '';
-        installPhase = ''
-          mkdir $out
-        '';
-        buildInputs = [
-          self.packages.x86_64-linux.homepage-test-application
-        ];
-      };
-
   };
 }
