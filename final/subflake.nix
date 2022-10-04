ace8eaa80dd32d6f39589edd783f43ad2a2d17fd
Felix Springer <felixspringer149@gmail.com>
2022-10-04 21:43:31 +0200

    Move nixos module to new nix file.

--- final/subflake.nix
+++ final/subflake.nix
@@ -20,50 +20,9 @@
     homepage-jumper149.config.default = config.config;
   };
 
-  nixosModules.default = { config, lib, pkgs, ... }:
-    {
-      options = {
-        nixpkgs.overlays = [
-          overlays.default
-        ];
-        services.homepage = {
-          enable = lib.mkEnableOption "Felix Springer's Homepage.";
-          config = lib.mkOption {
-            default = { };
-            type = with lib.types; attrsOf anything;
-            description = ''
-              Configuration, that will be merged with default options and serialized to JSON.
-              `lib.recursiveUpdate` is used to merge these changes.
-            '';
-          };
-        };
-      };
-      config = lib.mkIf config.services.homepage.enable {
-        environment = {
-          etc."homepage.json".text = builtins.toJSON (
-            lib.recursiveUpdate pkgs.homepage-jumper149.config.default config.services.homepage.config
-          );
-        };
-        systemd.services.homepage = {
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network.target" ];
-          description = "Homepage";
-          environment = {
-            HOMEPAGE_CONFIG_FILE = "/etc/homepage.json";
-            HOMEPAGE_LOG_FILE = "/var/log/homepage/access.log";
-            HOMEPAGE_LOG_LEVEL = "LevelInfo";
-          };
-          restartTriggers = [
-            config.environment.etc."homepage.json".source
-          ];
-          serviceConfig = {
-            DynamicUser = true;
-            ExecStart = "${pkgs.homepage-jumper149.exe}/bin/homepage";
-            LogsDirectory = "homepage";
-          };
-        };
-      };
-    };
+  nixosModules.default = import ./module.nix {
+    finalOverlay = overlays.default;
+  };
 
   checks.x86_64-linux.homepage-test-application =
     with import nixpkgs { system = "x86_64-linux"; overlays = [ setup.overlays.default ]; };
