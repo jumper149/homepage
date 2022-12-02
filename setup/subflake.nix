ba1a333cd22747a8572849cef8710625f69275ef
Felix Springer <felixspringer149@gmail.com>
2022-12-02 23:28:31 +0100

    Fix nix evaluation by disabling a test.

--- setup/subflake.nix
+++ setup/subflake.nix
@@ -5,6 +5,7 @@
       singletons = haskellPrev.callHackage "singletons" "3.0.1" {};
       singletons-th = haskellPrev.callHackage "singletons-th" "3.1" {};
       th-desugar = haskellPrev.callHackage "th-desugar" "1.13.1" {};
+      ListLike = prev.haskell.lib.dontCheck haskellPrev.ListLike; # TODO: Tests didn't finish.
       graphmod = (haskellPrev.graphmod.overrideAttrs (oldAttrs: {
         src = prev.fetchFromGitHub {
           owner = "yav";
