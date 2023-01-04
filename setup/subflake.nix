0b5f4713f1885cbada0dac8b4761a429e772e88e
Felix Springer <felixspringer149@gmail.com>
2023-01-04 15:56:39 +0100

    Remove overrides.
    Hopefully this actually works.

--- setup/subflake.nix
+++ setup/subflake.nix
@@ -2,10 +2,6 @@
 
   overlays.default = final: prev: {
     haskellPackages = prev.haskell.packages.ghc925.extend (haskellFinal: haskellPrev: { # TODO: Using GHC 9.2.5.
-      singletons = haskellPrev.callHackage "singletons" "3.0.1" {};
-      singletons-th = haskellPrev.callHackage "singletons-th" "3.1" {};
-      th-desugar = haskellPrev.callHackage "th-desugar" "1.13.1" {};
-      ListLike = prev.haskell.lib.dontCheck haskellPrev.ListLike; # TODO: Tests didn't finish.
       graphmod = (haskellPrev.graphmod.overrideAttrs (oldAttrs: {
         src = prev.fetchFromGitHub {
           owner = "yav";
