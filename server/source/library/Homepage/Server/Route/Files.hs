5d8d7d4b5b1c0b98df771825fd546b4d9cb6b06d
Felix Springer <felixspringer149@gmail.com>
2023-02-17 15:56:01 +0100

    Inline HTML definition for file overview.

--- server/source/library/Homepage/Server/Route/Files.hs
+++ server/source/library/Homepage/Server/Route/Files.hs
@@ -2,15 +2,24 @@ module Homepage.Server.Route.Files where
 
 import Homepage.Application.Configured.Class
 import Homepage.Configuration
+import Homepage.Configuration.Files
 import Homepage.Server.Err404
 import Homepage.Server.FileServer
+import Homepage.Server.Html.Depth
 import Homepage.Server.Html.Document
-import Homepage.Server.Html.Files
 import Homepage.Server.Route.Files.Type
 import Homepage.Server.Tab
 
 import Control.Monad.IO.Unlift
 import Control.Monad.Logger.CallStack
+import Data.Foldable
+import Data.List qualified as L
+import Data.Map qualified as M
+import Data.Maybe
+import Data.Ord
+import Data.Set qualified as S
+import Data.Text qualified as T
+import Data.Time.Calendar
 import Network.Wai.Trans
 import Servant
 import Servant.RawM.Server qualified as RawM
@@ -39,7 +48,29 @@ overviewHandler = do
   let description = (describeDocument contactInformation $ Just TabFiles) {documentDepth = Just 0}
   pure . document baseUrl contactInformation revision description $ do
     h2 "my Files"
-    fileList baseUrl (Just 0) fileEntries
+    let
+      entryGroups = groupFiles $ unFileEntries fileEntries
+      topLevelEntries = fromMaybe mempty $ M.lookup Nothing entryGroups
+      otherEntries = M.mapKeys fromJust . M.delete Nothing $ entryGroups
+      markupEntries entrySet = ul $ toMarkup $ fileToMarkup <$> L.sortOn (Down . fileTimestamp) (S.toList entrySet)
+       where
+        fileToMarkup file@FileEntry {Homepage.Configuration.Files.fileName, fileTimestamp} =
+          li $ do
+            toMarkup $ T.pack (showGregorian fileTimestamp)
+            " - "
+            toMarkup fileName
+            " [ "
+            toMarkup $ L.intersperse " | " $ fileFormat <$> fileFormats file
+            " ]"
+         where
+          fileFormat FileFormat {fileFormatName, fileFormatExtension} =
+            a ! hrefWithDepth baseUrl (Just 0) (textValue $ "files/" <> fileIdentifier file <> maybe "" ("." <>) fileFormatExtension) $ -- TODO: Use `Servant.Links`.
+              toMarkup fileFormatName
+    markupEntries topLevelEntries
+    let markupSection (sectionName, entrySet) = do
+          h2 $ text $ "my " <> sectionName
+          markupEntries entrySet
+    traverse_ markupSection $ M.toList otherEntries
 
 filesHandler ::
   (MonadConfigured m, MonadLogger m, MonadUnliftIO m) =>
