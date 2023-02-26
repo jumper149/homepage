b279c8d4c99694b3607531305a92d7d482f1839f
Felix Springer <felixspringer149@gmail.com>
2023-02-26 14:54:49 +0100

    Add id3 tags to music.

--- files/subflake.nix
+++ files/subflake.nix
@@ -8,8 +8,47 @@
       buildPhase = ''
       '';
       installPhase = ''
+        id3v2 --delete-all "Felix_Springer-Unitary.flac"
+        id3v2 --artist "Felix Springer" --song "Unitary" --year "2022" "Felix_Springer-Unitary.flac"
+
+        id3v2 --delete-all "Felix_Springer-Unitary.mp3"
+        id3v2 --artist "Felix Springer" --song "Unitary" --year "2022" "Felix_Springer-Unitary.mp3"
+
+        id3v2 --delete-all "Felix_Springer-Unitary.wav"
+        id3v2 --artist "Felix Springer" --song "Unitary" --year "2022" "Felix_Springer-Unitary.wav"
+
+        id3v2 --delete-all "Felix_Springer-Fido.flac"
+        id3v2 --artist "Felix Springer" --song "Fido" --year "2022" "Felix_Springer-Fido.flac"
+
+        id3v2 --delete-all "Felix_Springer-Fido.mp3"
+        id3v2 --artist "Felix Springer" --song "Fido" --year "2022" "Felix_Springer-Fido.mp3"
+
+        id3v2 --delete-all "Felix_Springer-Fido.wav"
+        id3v2 --artist "Felix Springer" --song "Fido" --year "2022" "Felix_Springer-Fido.wav"
+
+        id3v2 --delete-all "Felix_Springer-L2.flac"
+        id3v2 --artist "Felix Springer" --song "L2" --year "2023" "Felix_Springer-L2.flac"
+
+        id3v2 --delete-all "Felix_Springer-L2.mp3"
+        id3v2 --artist "Felix Springer" --song "L2" --year "2023" "Felix_Springer-L2.mp3"
+
+        id3v2 --delete-all "Felix_Springer-L2.wav"
+        id3v2 --artist "Felix Springer" --song "L2" --year "2023" "Felix_Springer-L2.wav"
+
+        id3v2 --delete-all "Felix_Springer-Naptune.flac"
+        id3v2 --artist "Felix Springer" --song "Naptune" --year "2023" "Felix_Springer-Naptune.flac"
+
+        id3v2 --delete-all "Felix_Springer-Naptune.mp3"
+        id3v2 --artist "Felix Springer" --song "Naptune" --year "2023" "Felix_Springer-Naptune.mp3"
+
+        id3v2 --delete-all "Felix_Springer-Naptune.wav"
+        id3v2 --artist "Felix Springer" --song "Naptune" --year "2023" "Felix_Springer-Naptune.wav"
+
         cp --recursive . $out
       '';
+      nativeBuildInputs = [
+        pkgs.id3v2
+      ];
     };
 
   entries = builtins.fromJSON (builtins.readFile ./entries.json);
