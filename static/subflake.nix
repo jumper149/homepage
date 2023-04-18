031503c09ee96ccee736da9ea21c2c9c723111f8
Felix Springer <felixspringer149@gmail.com>
2023-04-19 01:18:00 +0200

    Use self-hosted fonts.

--- static/subflake.nix
+++ static/subflake.nix
@@ -7,6 +7,8 @@
       src = ./.;
       buildPhase = ''
         make all
+
+        cp -r ${packages.x86_64-linux.fonts}/fonts build
       '';
       installPhase = ''
         cp --recursive build $out
@@ -16,6 +18,31 @@
       nativeBuildInputs = [
         imagemagick
         lessc
+      ];
+    };
+
+  packages.x86_64-linux.fonts =
+    with import nixpkgs { system = "x86_64-linux"; overlays = [ self.subflakes.setup.overlays.default ]; };
+    stdenv.mkDerivation {
+      name = "fonts"; # TODO: Necessary to avoid segmentation fault.
+      dontUnpack = true;
+      buildPhase = ''
+        mkdir -p build
+        cp -r ${pkgs.fira.outPath}/share/fonts/opentype build
+        mv build/opentype build/fonts
+        chmod --recursive +w build/fonts
+        for f in build/fonts/*
+        do
+          woff2_compress $f
+        done
+      '';
+      installPhase = ''
+        cp --recursive build $out
+      '';
+      buildInputs = [
+      ];
+      nativeBuildInputs = [
+        woff2
       ];
     };
 
